341403098aec0ce60c1573fd8745a6cdbf539845
skkzsh <skkzsh@myopera.com>
2012-12-22 22:45:01 +0900

    Setup Scripts

--- usr/setup/dot.ps1
+++ usr/setup/dot.ps1
@@ -5,10 +5,13 @@
 ### Setting
 
 ## 共有元の設定FilesのDirctory
-$unix_dir = "$ENV:HOMEPATH\Repository\bitbucket\unix_files"
-$prefix_dropbox = "$ENV:HOMEPATH\Dropbox"
-$setting = "$dropbox\setting"
-$vim = 'C:\vim73-kaoriya-win32'
+switch ($ENV:COMPUTERNAME) {
+    default           { $prefix = "$ENV:USERPROFILE" }
+}
+$unix_dir = "$prefix\Repository\bitbucket\unix_files"
+$dropbox = "$prefix\Dropbox\setting"
+#$vim = 'C:\vim73-kaoriya-win32'
+$vim_prefix = "$ENV:HOME"
 
 ## 共有元の各Directoryで共有させるFilesのList
 $bashrc = '.profile', '.bashrc', '.bash_aliases', '.inputrc', '.bash_env'
@@ -34,27 +37,30 @@ foreach ($dir in $dot_emacs_d) {
 
 ## Vim
 foreach ($file in $vimrc) {
-    smartln.ps1 mklink "$unix_dir\.$file" "$vim\_$file"
+    smartln.ps1 mklink "$unix_dir\.$file" "$vim_prefix\_$file"
 }
-smartln.ps1 mklink "$unix_dir\.vim" "$vim\vimfiles"
+smartln.ps1 mklink "$unix_dir\.vim" "$vim_prefix\vimfiles"
 
 ## Dot Files
-foreach ($file in $bashrc, $dot_unix_files, $dot_emacs) {
-    smartln.ps1 mklink "$unix_dir\$file" "$ENV:HOME\$file"
-}
+#foreach ($file in $bashrc, $dot_unix_files, $dot_emacs) {
+#    smartln.ps1 mklink "$unix_dir\$file" "$ENV:HOME\$file"
+#}
 ## Dot Directories
 foreach ($dir in $dot_unix_dirs) {
     smartln.ps1 mklink "$unix_dir\$file" "$ENV:HOME\$file"
 }
 
 ## SSH
-smartln.ps1 mklink "$unix_dir\.ssh\config" "$ENV:HOME\.ssh\config"
+#smartln.ps1 mklink "$unix_dir\.ssh\config" "$ENV:HOME\.ssh\config"
+
+## texmf
+smartln.ps1 mklink "$unix_dir/share/texmf" "$ENV:USERPROFILE/texmf"
 
 ## Vimperator
 foreach ($file in .vimperatorrc .muttatorrc) {
-    smartln.ps1 mklink "$setting\$file" "$ENV:HOME\$file"
+    smartln.ps1 mklink "$dropbox\$file" "$ENV:HOME\$file"
 }
-smartln.ps1 mklink "$setting\.vimperator\plugin" "$ENV:HOME\vimperator\plugin"
-smartln.ps1 mklink "$setting\.vimperator\conf" "$ENV:HOME\vimperator\conf"
+smartln.ps1 mklink "$dropbox\.vimperator\plugin" "$ENV:HOME\vimperator\plugin"
+smartln.ps1 mklink "$dropbox\.vimperator\conf" "$ENV:HOME\vimperator\conf"
 
 exit 0
--- usr/setup/dot.sh
+++ usr/setup/dot.sh
@@ -8,29 +8,32 @@
 
 ### Setting
 
+## X11の有無
+x11=no
+
 ## 共有元の設定FilesのDirctory
 unix_dir=~/Repository/bitbucket/unix_files
 
 ## 共有元の各Directoryで共有させるFilesのList
 
-bashrc='.bashrc .bash_aliases .inputrc .bash_env'
-zshrc='.zshrc .zprofile .zsh_aliases'
-dot_zsh='plugin functions'
+bashrc='.bash_profile .bashrc .inputrc .bash'
+zshrc='.zshrc .zprofile'
+dot_zsh='plugin functions conf'
 
-dot_vim='.vimrc .gvimrc .vim'
+dot_vim='.vimrc .vim'
 dot_emacs='.emacs-w3m .wl .folders .signature'
 dot_emacs_d='init.el conf lisp elpa auto-install public_repos'
 
-dot_unix='.screenrc .gitconfig .gitignore .perltidyrc .latexmkrc .auto-insert .percol.d bin'
-dot_linux='.conkyrc .apvlvrc'
+dot_unix='.screenrc .gitconfig .gitignore .perltidyrc .latexmkrc .auto-insert .percol.d'
 
-dot_x11='.Xresources .Xmodmap .xmonad'
+dot_x11='.gvimrc .conkyrc .apvlvrc .Xresources .Xmodmap .xmonad'
 dot_fluxbox='init styles keys startup apps'
 
 
 ## rmのOption
 alias rm='rm -i'
 ## Symbolic Linkを張る関数の読み込み
+[ -d ~/bin ] || (ln -s $unix_dir/bin ~/bin; echo 'bin Linked')
 . ~/bin/SmartLn.sh
 
 
@@ -41,16 +44,8 @@ for file in $bashrc $zshrc $dot_unix $dot_emacs $dot_vim; do
     SmartLn ln $unix_dir/$file ~/$file
 done
 
-## TODO: .profile
-if [ -f ~/.profile ]; then
-    # Debian
-    SmartLn ln $unix_dir/.profile ~/.profile
-elif [ -f ~/.bash_profile ]; then
-    # Mac Redhat
-    SmartLn ln $unix_dir/.profile ~/.bash_profile
-fi
-
 ## ~/.emacs.d
+[ -d ~/.emacs.d ] || (mkdir ~/.emacs.d; echo '~/.emacs.d Created')
 for file in $dot_emacs_d; do
     SmartLn ln $unix_dir/.emacs.d/$file ~/.emacs.d/$file
 done
@@ -60,33 +55,41 @@ if [ -d /mnt/hgfs ]; then
 fi
 
 ## ~/.zsh
+[ -d ~/.zsh ] || (mkdir ~/.zsh; echo '~/.zsh Created')
 for file in $dot_zsh; do
     SmartLn ln $unix_dir/.zsh/$file ~/.zsh/$file
 done
 
 ## SSH
+[ -d ~/.ssh ] || (mkdir ~/.ssh; echo '~/.ssh Created')
 SmartLn ln $unix_dir/.ssh/config ~/.ssh/config
-# TEXMF
-SmartLn ln $unix_dir/share/texmf ~/texmf
 
 ## Vimperator
-SmartLn ln $set_dir/.vimperatorrc ~/.vimperatorrc
-SmartLn ln $set_dir/.vimperator/plugin ~/.vimperator/plugin
-SmartLn ln $set_dir/.vimperator/conf ~/.vimperator/conf
+# SmartLn ln $set_dir/.vimperatorrc ~/.vimperatorrc
+# SmartLn ln $set_dir/.vimperator/plugin ~/.vimperator/plugin
+# SmartLn ln $set_dir/.vimperator/conf ~/.vimperator/conf
 
 
 ## OSごとのSetting
-case `uname` in
+case "`uname`" in
     Linux)
-        for file in $dot_linux $dot_x11; do
-            SmartLn ln $unix_dir/$file ~/$file
-        done
-        ## Fluxbox
-        for file in $dot_fluxbox; do
-            SmartLn ln $unix_dir/.fluxbox/$file ~/.fluxbox/$file
-        done
+        SmartLn ln $unix_dir/share/texmf ~/texmf
+
+        if [ $x11 = yes ]; then
+            for file in $dot_x11; do
+                SmartLn ln $unix_dir/$file ~/$file
+            done
+            ## Fluxbox
+            # if [ ! -d ~/.fluxbox ]; then
+            #     mkdir ~/.fluxbox
+            # fi
+            for file in $dot_fluxbox; do
+                SmartLn ln $unix_dir/.fluxbox/$file ~/.fluxbox/$file
+            done
+        fi
         ;;
     Darwin)
+        SmartLn ln $unix_dir/share/texmf ~/Library/texmf
         ;;
     *)  ;;
 esac
@@ -97,7 +100,6 @@ if [ -f /etc/issue.net ]; then
         Ubuntu*)
             ;;
         Debian*)
-            SmartLn ln $unix_dir/.debian.xsessionrc ~/.xsessionrc
             ;;
         *)  ;;
     esac
@@ -108,20 +110,20 @@ fi
 case "$HOSTNAME" in
     sing*)
         ;;
-    drive*)
-        # SmartLn ln $unix_dir/.Xmodmap ~/.xmodmaprc
+    box*)
         ;;
-    leap*)
+    over*)
         SmartLn ln $unix_dir/.leap.xprofile ~/.xprofile
-        SmartLn ln $unix_dir/.Xmodmap ~/.xmodmaprc
+        SmartLn ln $unix_dir/.Xmodmap ~/.Xmodmap
+        # SmartLn ln $unix_dir/.Xmodmap ~/.xmodmaprc
         # SmartLn ln $unix_dir/.xmonad.xsessionrc ~/.xsessionrc
         # SmartLn ln $set/.gconf/apps/gnome-terminal ~/.gconf/apps/gnome-terminal
         # SmartLn ln $unix_dir/.synergys/leap/esprimo-leap.synergy.conf ~/.synergy.conf
         ;;
-    box*)
-        ;;
     ride*)
-        SmartLn ln $unix_dir/.kuins.bash_proxy ~/.bash_proxy
+        SmartLn ln $unix_dir/.bash/conf/proxy.bash ~/.bash_proxy
+        ;;
+    ubuntu*)
         ;;
     debian*)
         ;;
