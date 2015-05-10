6acc66fbe0ebe00eb9bce3720f0d70f0405921c6
skkzsh <skkzsh12@gmail.com>
2015-05-10 19:27:17 +0900

    Modify plugin config

--- .bash/conf/plugin-init.bash
+++ .bash/conf/plugin-init.bash
@@ -2,29 +2,85 @@
 # NOTE: source this file in .zshrc
 
 #---------------------------------------------------------------------------
-### percol
-if which percol > /dev/null 2>&1 ; then
+### peco
+if which peco > /dev/null 2>&1 ; then
 
-    ppgrep() {
-        if [[ $1 == "" ]]; then
-            PERCOL=percol
+    function peco_lscd() {
+        local dir="$(find . -maxdepth 1 -type d | sed 's;\./;;' | peco)"
+        [[ -n $dir ]] && cd "$dir"
+    }
+    # bind -x '"\C-x\C-f": peco_find'
+
+    function peco_pgrep() {
+        if [[ $1 == "" ]] ; then
+            PECO=peco
         else
-            PERCOL="percol --query $1"
+            PECO="peco --query $1"
         fi
-        ps aux | eval $PERCOL | awk '{print $2}'
+        ps aux | eval $PECO | awk '{print $2}'
     }
 
-    ppkill() {
-        if [[ $1 =~ "^-" ]]; then
+    function peco_pkill() {
+        if [[ $1 =~ "^-" ]] ; then
             QUERY=""            # options only
         else
             QUERY=$1            # with a query
             [[ $# > 0 ]] && shift
         fi
-        ppgrep $QUERY | xargs kill $*
+        peco_pgrep $QUERY | xargs kill $*
     }
 
+    if [[ -n $BASH ]]; then
+
+        if (which tac || which gtac) > /dev/null 2>&1 ; then
+            function peco_select_history_b() {
+                local tac
+                if which gtac > /dev/null 2>&1 ; then
+                    tac=gtac
+                else
+                    tac=tac
+                fi
+                READLINE_LINE=$(cat ~/.bash_history | eval $tac | peco --query "$READLINE_LINE")
+                READLINE_POINT=$#READLINE_LINE
+            }
+            bind -x '"\C-[r": peco_select_history_b'
+        fi
+
+        # Git
+        # function peco_git_add_b {
+        # }
+        # function peco_git_modified_file_b {
+        # }
+        # bind -x '"\C-x\C-f": peco_git_modified_file_b'
+
+    fi
+
 fi
+
+#---------------------------------------------------------------------------
+### percol
+# if which percol > /dev/null 2>&1 ; then
+#
+#     function percol_pgrep() {
+#         if [[ $1 == "" ]] ; then
+#             PERCOL=percol
+#         else
+#             PERCOL="percol --query $1"
+#         fi
+#         ps aux | eval $PERCOL | awk '{print $2}'
+#     }
+#
+#     function percol_pkill() {
+#         if [[ $1 =~ "^-" ]] ; then
+#             QUERY=""            # options only
+#         else
+#             QUERY=$1            # with a query
+#             [[ $# > 0 ]] && shift
+#         fi
+#         percol_pgrep $QUERY | xargs kill $*
+#     }
+#
+# fi
 
 #---------------------------------------------------------------------------
 ### autojump
@@ -34,28 +90,12 @@ if which brew > /dev/null 2>&1 && [[ -d $(brew --prefix autojump) ]] ; then
     . $(brew --prefix)/etc/autojump.sh
 elif [[ -f ~/.autojump/etc/profile.d/autojump.sh ]] ; then
     . ~/.autojump/etc/profile.d/autojump.sh
-    # v20.9 on Windows
 fi
 
 #---------------------------------------------------------------------------
 ### z
-# case "$(uname)" in
-#     Linux)
-#         ;;
-#
-#     Darwin)
-#        ## brew info z.sh
-#         if which brew > /dev/null ; then
-#             if [[ -d $(brew --prefix z) ]]; then
-#                 . $(brew --prefix)/etc/profile.d/z.sh
-#                 if [[ -n $ZSH_NAME ]]; then
-#                     precmd () {
-#                     z --add "$(pwd -P)"
-#                 }
-#             fi
-#         fi
-#
-#     fi
-#     ;;
-# *) ;;
-# esac
+## brew info z.sh
+if which brew > /dev/null 2>&1 && [[ -d $(brew --prefix z) ]] ; then
+    # _Z_CMD=j
+    . $(brew --prefix)/etc/profile.d/z.sh
+fi
